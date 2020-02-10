381c1d45ee7546bb6770cd298f6e7f8957bf4ae2
StevenSun121 <StevenSun121@gmail.com>
2020-02-10 13:54:44 +0800

    重构
    修改原来的引用方式,改为模块化链式调用

--- common/js/domUtils.js
+++ common/js/domUtils.js
@@ -1,48 +1,109 @@
-//根据ID查询node
-function ID(id) {
-    return document.getElementById(id)
+/**
+ * 链式 操作页面元素 方法
+ */
+
+function $(id) {
+    let node = typeof id == "string" ? document.getElementById(id) : id
+    return new domUtils(node)
 }
 
-//显示隐藏节点
-function show(id) {
-    let node = typeof id == "string" ? ID(id) : id
-    node.classList.remove("hide")
-}
-
-//隐藏节点
-function hide(id) {
-    let node = typeof id == "string" ? ID(id) : id
-    node.classList.add("hide")
-}
-
-// 显示 / 隐藏节点
-function toggle(id, className) {
-    let node = typeof id == "string" ? ID(id) : id
-    node.classList.toggle(className ? className : "hide")
-}
-
-//添加向节点末尾添加HTML
-function appendHTML(node, html, clickCallback) {
-    let temp = document.createElement("div")
-    temp.innerHTML = html
-    targetNode = temp.lastElementChild
-    if(clickCallback) {
-        targetNode.onclick = clickCallback
+class domUtils {
+    constructor(node) {
+        this.node = node;
+        ( "blur focus focusin focusout resize scroll click dblclick " +
+        "mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave " +
+        "change select submit keydown keypress keyup contextmenu" ).split( " " ).forEach((item, index) => {
+            this[item] = callback => {
+                let event = "on" + item
+                callback ? this.node[event] = callback : this.node[event]()
+            }
+        })
+        return this;
     }
-    node.appendChild(targetNode)
+    //隐藏节点
+    hide() {
+        this.addClass("hide")
+        return this
+    }
+    //显示节点
+    show() {
+        this.removeClass("hide")
+        return this
+    }
+    //切换 样式
+    toggle(className = "hide") {
+        this.node.classList.toggle(className)
+        return this
+    }
+    //是否显示
+    isShow() {
+        return !this.node.classList.contains("hide")
+    }
+    //添加样式
+    addClass(className) {
+        this.node.classList.add(className)
+    }
+    //移除样式
+    removeClass(className) {
+        this.node.classList.remove(className)
+    }
+    //添加向节点末尾添加子节点
+    append(html, clickCallback) {
+        let temp = document.createElement("div")
+        temp.innerHTML = html
+        let targetNode = temp.lastElementChild
+        if(clickCallback) {
+            targetNode.onclick = clickCallback
+        }
+        this.node.appendChild(targetNode)
+        return this
+    }
+    // 当前节点的兄弟节点
+    siblings(callback) {
+        this.node.parentElement.childNodes.forEach(callback)
+    }
+    //获取/设置节点参数
+    attr(name, value) {
+        if(value || value == "") {
+            this.node.setAttribute(name, value)
+            return this
+        }else {
+            return this.node.getAttribute(name)
+        }
+    }
+    //修改节点html代码
+    html(html) {
+        if(html == "" || html == "0" || html) {
+            this.node.innerHTML = html
+            return this
+        }else {
+            return this.node.innerHTML
+        }
+    }
+    //修改节点 文本
+    text(text) {
+        this.node.innerText = text
+        return this
+    }
+    //重定向到当前节点的父节点
+    parent() {
+        this.node = this.node.parentElement
+        return this
+    }
+    //滚动当前节点
+    scrollTop(scrollTop) {
+        this.node.scrollTop = scrollTop
+        return this
+    }
 }
 
-// 当前节点的兄弟节点
-function siblings(node, callback) {
-    node.parentElement.childNodes.forEach(callback)
+//数字补位
+$.numFill = function(number, length = 2, fill = "0") {
+    return (Array(length).join(fill) + number).slice(-length)
 }
 
-//数字补位 一位数变两位数
-function numAddZero(number) {
-    return (Array(2).join(0) + number).slice(-2)
-}
-
-var keyCode = {
+//键盘代码
+$.keyCode = {
     "BackSpace"     : 8,
     "Tab"           : 9,
     "Clear"         : 12,
@@ -157,3 +218,5 @@ var keyCode = {
     "]}"            : 221,
     "'\""           : 222
 }
+
+module.exports = $
