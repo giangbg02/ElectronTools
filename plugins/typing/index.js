fca77921c2831a8ef083bdabc508ae82f5837007
StevenSun121 <StevenSun121@gmail.com>
2020-02-15 23:48:14 +0800

    创建typing包

---
+++ plugins/typing/index.js
@@ -0,0 +1,39 @@
+const {remote, ipcRenderer} = require('electron')
+
+const config = require('../../common/js/config').file('plugins/_template');
+
+const $ = require('../../common/js/domUtils')
+
+const dict = require('./dict')
+
+let aa = "和"
+
+
+
+/**
+ * 标题栏按钮
+ */
+
+//设置
+// document.getElementById("setting").onclick = toggleSetting
+
+//最小化
+$("min").click(_ => {
+    remote.getCurrentWindow().minimize()
+})
+//最大化
+$("max").click(_ => {
+    max.classList.toggle("hide")
+    resize.classList.toggle("hide")
+    remote.getCurrentWindow().maximize()
+})
+//取消最大化
+$("resize").click(_ => {
+    max.classList.toggle("hide")
+    resize.classList.toggle("hide")
+    remote.getCurrentWindow().unmaximize()
+})
+//关闭
+$("close").click(_ => {
+    remote.getCurrentWindow().hide()
+})
